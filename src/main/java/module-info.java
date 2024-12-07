6dc5852befe52e6b21c868b2020d95ad4de735db
ParaN3xus <paran3xus007@gmail.com>
2024-12-07 22:52:39 +0800

    deps: jfx utils

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,9 @@ module pkg.paran3xus.dou {
     requires javafx.fxml;
     requires org.java_websocket;
     requires com.google.gson;
+    requires javafx.graphics;
+    requires java.desktop;
+    requires javafx.swing;
 
     opens pkg.paran3xus.dou to javafx.fxml;
 
